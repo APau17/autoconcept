963e95011d28033c1d85ed5685d6f35bbf00ee7e
Adrien Jeser <adrien@jeser.me>
2017-04-10 03:46:40 +0200

    Categorie (#1)

---
+++ 3-stock/creation/3-categorie.sql
@@ -0,0 +1,20 @@
+#------------------------------------------------------------
+# Table: Categorie Piece
+#------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Categorie_piece (
+        id Int Auto_increment PRIMARY KEY
+        COMMENT 'Clef primaire',
+
+        nom Varchar (50) NOT NULL,
+
+        lft Int
+        COMMENT 'Minimal interval enfants',
+
+        rgt Int
+        COMMENT 'Maximal interval enfants',
+
+        # Keys
+        Modele_de_piece_id Int,
+        FOREIGN KEY (Modele_de_piece_id) REFERENCES Modele_de_piece(id)
+);
