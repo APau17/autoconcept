5a58d4f0de9ed8140917800c274d52057888d8bd
kells64000 <kelian.bousquet@laposte.net>
2017-04-12 01:45:18 +0200

    Jeu de donnee categorie_piece emplacement compatibilite + modification generateur plaque immatriculation

---
+++ 2-stock/jeu_de_donnees/compatibilite.sql
@@ -0,0 +1,6 @@
+INSERT INTO Compatibilite(id, Modele_de_voiture_id ) VALUES (1, 1);
+INSERT INTO Compatibilite(id, Modele_de_voiture_id ) VALUES (2, 2);
+INSERT INTO Compatibilite(id, Modele_de_voiture_id ) VALUES (3, 3);
+INSERT INTO Compatibilite(id, Modele_de_voiture_id ) VALUES (4, 4);
+INSERT INTO Compatibilite(id, Modele_de_voiture_id ) VALUES (5, 5);
+INSERT INTO Compatibilite(id, Modele_de_voiture_id ) VALUES (6, 6);
