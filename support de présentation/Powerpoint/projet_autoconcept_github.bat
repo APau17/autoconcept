bc6b07942eeaaafda90fe2bc9cb7fd24d6c25c99
kells64000 <kelian.bousquet@laposte.net>
2017-04-12 14:28:25 +0200

    lien vers la vidéo youtube

--- support de présentation/Powerpoint/projet_autoconcept_github.bat
+++ support de présentation/Powerpoint/projet_autoconcept_github.bat
@@ -1,2 +1,2 @@
 @echo off
-start firefox.exe "https://framadrop.org/r/u_NEbyfGa6#8A/32IJIn5nwK/inrp2/Ht3WmaCQ2a2PEz+PcEKIvC8="
+start firefox.exe "https://www.youtube.com/watch?v=K177Ra6JfMg&feature=youtu.be"
